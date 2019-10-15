748ae65604e65891ec946059b1e911da7ec3a7a1
Peter <peterisdew@gmail.com>
2019-10-15 15:44:13 +1300

    Level wall Generation

--- New Unity Project/Assets/scripts/GameManager.cs
+++ New Unity Project/Assets/scripts/GameManager.cs
@@ -8,12 +8,15 @@ public class GameManager : MonoBehaviour
     public GameObject two;
     public GameObject four;
 
+    public GameObject floor;
+
     public int sizeX = 10;
     public int sizeZ = 10;
 
+    float xfloat;
+    float zfloat;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         if (sizeX % 2 != 0)
         {
@@ -23,7 +26,8 @@ public class GameManager : MonoBehaviour
         {
             sizeZ = sizeZ + 1;
         }
-
+        xfloat = sizeX;
+        zfloat = sizeZ;
         Boarder();
     }
 
@@ -34,25 +38,26 @@ public class GameManager : MonoBehaviour
 
     public void Boarder()
     {
-       
+        floor.transform.localScale = new Vector3(xfloat / 10f, 1f, zfloat / 10f);
+        Instantiate(floor, transform.position + new Vector3(0f, -0.5f, 0f), Quaternion.identity);
+
         for (int i = 0; i < sizeX; i++)
         {
-            Vector3 pos = new Vector3(i - (sizeX / 2)+ 0.5f, 0, (-sizeZ / 2)+0.5f);
-            Vector3 pos2 = new Vector3(i - (sizeX / 2) + 0.5f, 0, (sizeZ / 2)-0.5f);
+            Vector3 pos = new Vector3(i - (sizeX / 2)+ 0.5f, 0.5f, (-sizeZ / 2)+0.5f);
+            Vector3 pos2 = new Vector3(i - (sizeX / 2) + 0.5f, 0.5f, (sizeZ / 2)-0.5f);
             Instantiate(one, pos, transform.rotation);
             Instantiate(one, pos2, transform.rotation);
         }
 
         for (int i = 0; i < sizeZ -2; i++)
         {
-            Vector3 pos = new Vector3((-sizeX / 2) +0.5f, 0, i - ((sizeZ-2) / 2) + 0.5f);
-            Vector3 pos2 = new Vector3((sizeX / 2) - 0.5f, 0, i - ((sizeZ - 2) / 2) + 0.5f);
+            Vector3 pos = new Vector3((-sizeX / 2) +0.5f, 0.5f, i - ((sizeZ-2) / 2) + 0.5f);
+            Vector3 pos2 = new Vector3((sizeX / 2) - 0.5f, 0.5f, i - ((sizeZ - 2) / 2) + 0.5f);
             Instantiate(one, pos, transform.rotation);
             Instantiate(one, pos2, transform.rotation);
-        }
-
+        }              
     }
-    // Update is called once per frame
+
     void Update()
     {
         
