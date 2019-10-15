149f079e49e7d1686c8e7e40d7f66771dc1786e8
Peter <peterisdew@gmail.com>
2019-10-15 18:21:17 +1300

    better

--- New Unity Project/Assets/scripts/GameManager.cs
+++ New Unity Project/Assets/scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     public GameObject four;
 
     public GameObject floor;
+    public GameObject mainCamera;
+    public GameObject level;
 
     public int sizeX = 10;
     public int sizeZ = 10;
@@ -29,37 +31,37 @@ public class GameManager : MonoBehaviour
         xfloat = sizeX;
         zfloat = sizeZ;
         Boarder();
-    }
 
-    public void LevelSetup()
-    {
-        
+        if(sizeX > sizeZ)
+        {
+            mainCamera.transform.position = new Vector3(0, xfloat/2f +zfloat/10, 0);
+        }
+        if(sizeX<sizeZ)
+        {
+            mainCamera.transform.position = new Vector3(0, zfloat/2f + zfloat/10f, 0);
+        }
     }
 
     public void Boarder()
     {
         floor.transform.localScale = new Vector3(xfloat / 10f, 1f, zfloat / 10f);
-        Instantiate(floor, transform.position + new Vector3(0f, -0.5f, 0f), Quaternion.identity);
+        Instantiate(floor, transform.position, Quaternion.identity);
 
         for (int i = 0; i < sizeX; i++)
         {
             Vector3 pos = new Vector3(i - (sizeX / 2)+ 0.5f, 0.5f, (-sizeZ / 2)+0.5f);
             Vector3 pos2 = new Vector3(i - (sizeX / 2) + 0.5f, 0.5f, (sizeZ / 2)-0.5f);
-            Instantiate(one, pos, transform.rotation);
-            Instantiate(one, pos2, transform.rotation);
+            Instantiate(one, pos, transform.rotation, level.transform);            
+            Instantiate(one, pos2, transform.rotation, level.transform);
+
         }
 
         for (int i = 0; i < sizeZ -2; i++)
         {
             Vector3 pos = new Vector3((-sizeX / 2) +0.5f, 0.5f, i - ((sizeZ-2) / 2) + 0.5f);
             Vector3 pos2 = new Vector3((sizeX / 2) - 0.5f, 0.5f, i - ((sizeZ - 2) / 2) + 0.5f);
-            Instantiate(one, pos, transform.rotation);
-            Instantiate(one, pos2, transform.rotation);
+            Instantiate(one, pos, transform.rotation, level.transform);
+            Instantiate(one, pos2, transform.rotation, level.transform);
         }              
-    }
-
-    void Update()
-    {
-        
     }
 }
